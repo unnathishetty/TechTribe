b93b3f1297d63f335b023fe019b1bac30e46c5c7
Unnathi U Shetty <117020426+unnathishetty@users.noreply.github.com>
2023-06-02 05:30:04 -0400

    Update eregister.php

--- eregister.php
+++ eregister.php
@@ -6,13 +6,13 @@ $name=$_POST['nam'];
 $uname=$_POST['un'];
 $pass=$_POST['pas'];
 $em=$_POST['email'];
-$cty=$_POST['city'];
-$g=$_POST['branch'];
+$br=$_POST['branch'];
+$g=$_POST['gender'];
 $ag=$_POST['age'];
 $conno=$_POST['cn'];
 
 
-$sql="INSERT INTO euser (e_id,e_name,e_branch,e_age,e_gender,e_email,e_conno,e_username,e_password) VALUES('$id','$name','$cty','$ag','$g','$em','$conno','$uname','$pass')";
+$sql="INSERT INTO euser (e_id,e_name,e_branch,e_age,e_gender,e_email,e_conno,e_username,e_password) VALUES('$id','$name','$br','$ag','$g','$em','$conno','$uname','$pass')";
 mysqli_query($con,$sql);
 header('location:elogin.html');
 ?>
