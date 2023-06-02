f691b8f87f34f4f26e0d1379a8e2013e7121d025
Unnathi U Shetty <117020426+unnathishetty@users.noreply.github.com>
2023-06-02 05:28:54 -0400

    Update elogin.php

--- elogin.php
+++ elogin.php
@@ -9,7 +9,7 @@ $password = $_POST['pass'];
 $password = mysqli_real_escape_string($con, $password);
 
 // Query checks if the email and password are present in the database. 
-$query = "SELECT * FROM euser WHERE e_username='" . $adminid . "' and se_password='".$password."'";
+$query = "SELECT * FROM euser WHERE e_username='" . $adminid . "' and e_password='".$password."'";
 
 $result = mysqli_query($con, $query)or die($mysqli_error($con));
 $num = mysqli_num_rows($result);
@@ -19,7 +19,7 @@ if ($num == 0) {
   header('location: al.php?error=' . $error);
 } else {  
   $row = mysqli_fetch_array($result);
-  $_SESSION['adminid'] = $row['se_username'];
+  $_SESSION['adminid'] = $row['e_username'];
   $_SESSION['id'] = $row['e_id'];
   $_SESSION['name'] = $row['e_name'];
   header('location: elogin.php');
